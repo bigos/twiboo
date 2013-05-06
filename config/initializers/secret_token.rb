836334ad05bc64d806c8990107b0746b0d8824c2
Jacek <ruby.object@googlemail.com>
2013-05-06 01:11:58 +0100

    new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twiboo::Application.config.secret_token = 'ccc2b385f5fa7a12de1921e212d8b8afab95f8d3c07fc0e7a1ed39eb10ffdcdedc76b13832600f442cbb6db56f3f52fa655d2ef91e5bd5b9936e6873dce3f8b7'
